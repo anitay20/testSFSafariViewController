fb31581e7aa1522256ecabe16a941eb9d598d36a
anita <anitay20@gmail.com>
2018-01-16 11:43:38 -0800

    chore: remove comments and add code for testTapTextField

--- TestSFSafariViewControllerUITests/TestSFSafariViewControllerUITests.swift
+++ TestSFSafariViewControllerUITests/TestSFSafariViewControllerUITests.swift
@@ -2,35 +2,32 @@
 //  TestSFSafariViewControllerUITests.swift
 //  TestSFSafariViewControllerUITests
 //
-//  Created by anita on 1/12/18.
-//  Copyright © 2018 slice. All rights reserved.
-//
 
 import XCTest
 
 class TestSFSafariViewControllerUITests: XCTestCase {
-        
+    
+    let app = XCUIApplication()
+    
     override func setUp() {
         super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
     func testExample() {
         
-       
     }
     
+    func testTapTextField() {
+        app.buttons["Go to Yahoo"].tap()
+        sleep(5)        // Make sure the page loads
+        let textField = app.webViews.textFields.firstMatch
+        print("app.debugDescription: \n\(app.debugDescription)")
+        textField.tap()
+    }
 }
